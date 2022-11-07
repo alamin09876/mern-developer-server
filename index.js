c530ac88cca5bb0c77a518ac8888e24c5614b324
alamin09876 <alamin.ahmed0795@gmail.com>
2022-11-08 01:52:59 +0600

    data load

--- index.js
+++ index.js
@@ -3,31 +3,38 @@ const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 5000
+require('dotenv').config();
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-// mernUser
-// 
 
-// console.log(process.env.DB_USER)
-// ${process.env.DB_USER} ${process.env.DB_PASSWORD}
-
-
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dotnzuk.mongodb.net/?retryWrites=true&w=majority";
-console.log(uri)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dotnzuk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+    try {
+        const serviceCollection = client.db('mernUser').collection('services')
+       
+
+       
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
-  res.send('mern developer data loading!')
+    res.send('mern developer data loading!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
