d92d72a33aa457081e9a6e892bcc029a954daacb
alamin09876 <alamin.ahmed0795@gmail.com>
2022-11-08 04:19:53 +0600

    load data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = 5000
 require('dotenv').config();
 
@@ -17,14 +17,21 @@ async function run() {
         const serviceCollection = client.db('mernUser').collection('services')
        
 
-       
         app.get('/services', async(req, res) =>{
             const query = {}
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray();
             res.send(services)
         })
+        app.get('/services/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+        
     }
+
     finally {
 
     }
